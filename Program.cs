18307b8897cb200cc5d3d6d8e84bfa992eba5e49
lucasbastosf <lucasbastos1987@gmail.com>
2024-07-27 13:29:48 -0300

    calculo de media aluno

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace NotaAluno
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite a nota do aluno: ");
+            double nota = Convert.ToDouble(Console.ReadLine());
+
+            if (nota < 5)
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+            else if (nota >= 5 && nota < 7)
+            {
+                Console.WriteLine("Aluno em exame.");
+            }
+            else
+            {
+                Console.WriteLine("Aluno aprovado.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
